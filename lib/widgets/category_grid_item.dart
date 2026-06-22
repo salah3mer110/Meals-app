f9918ac7f743594b6e7a499babaf44c0c52b0674
salah3mer110 <salahamer3040@gmail.com>
2026-06-22 07:58:02 +0300

    add Category screen

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(30),
+
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: .55),
+              category.color.withValues(alpha: .9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurfaceVariant,
+          ),
+        ),
+      ),
+    );
+  }
+}
